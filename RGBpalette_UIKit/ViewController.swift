f2a1b38a93c87090b7c40ba5cf94ebe48b7cb1f5
Artsiom <artiklol2012@gmail.com>
2022-10-11 19:33:21 +0300

    Fix bugs

--- RGBpalette_UIKit/ViewController.swift
+++ RGBpalette_UIKit/ViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet var rgbPalette: UIView!
     
     @IBOutlet var redLabel: UILabel!
@@ -25,68 +25,100 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+       
         setupRgbPalette()
-        setupSlider()
-        setupLabel()
-        backgroundСoloring()
-    }
 
+        setupSlider(slider: redSlider, color: .red)
+        setupSlider(slider: greenSlider, color: .green)
+        setupSlider(slider: blueSlider, color: .blue)
+        
+        setupKeyboard(textField: redTextField)
+        setupKeyboard(textField: greenTextField)
+        setupKeyboard(textField: blueTextField)
+        
+        updatingValuesRGB()
+    }
+    
     @IBAction func redSliderAction() {
-        setupLabel()
-        backgroundСoloring()
+        updatingValuesRGB()
     }
     
     @IBAction func greenSliderAction() {
-        setupLabel()
-        backgroundСoloring()
+        updatingValuesRGB()
     }
     
     @IBAction func blueSliderAction() {
-        setupLabel()
-        backgroundСoloring()
+        updatingValuesRGB()
     }
-
+    
     private func setupRgbPalette(){
         rgbPalette.layer.cornerRadius = 15
     }
     
-    private func setupSlider(){
-        redSlider.value = 0
-        redSlider.minimumValue = 0
-        redSlider.maximumValue = 1
-        
-        greenSlider.value = 0
-        greenSlider.minimumValue = 0
-        greenSlider.maximumValue = 1
-        
-        blueSlider.value = 0
-        blueSlider.minimumValue = 0
-        blueSlider.maximumValue = 1
+    private func setupSlider(slider: UISlider, color: UIColor?){
+        slider.value = 0
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = color
+        slider.maximumTrackTintColor = .lightGray
+        slider.thumbTintColor = .white
     }
     
-    private func setupLabel(){
+    private func updatingValuesRGB(){
         redLabel.text = "Red:    " + String(format: "%.2f", redSlider.value)
-        
         greenLabel.text = "Green: " + String(format: "%.2f", greenSlider.value)
-        
         blueLabel.text = "Blue:    " + String(format: "%.2f", blueSlider.value)
         
         redTextField.text = String(format: "%.2f", redSlider.value)
-        
         greenTextField.text = String(format: "%.2f", greenSlider.value)
-        
         blueTextField.text = String(format: "%.2f", blueSlider.value)
+        
+        rgbPalette.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                             green: CGFloat(greenSlider.value),
+                                             blue: CGFloat(blueSlider.value),
+                                             alpha: CGFloat(1))
     }
     
-    private func setupTextField(){
+    private func setupKeyboard(textField: UITextField){
+        textField.delegate = self
+        textField.keyboardType = .decimalPad
         
+        //add ToolBar and button "Done"
+        let bar = UIToolbar()
+        let done = UIBarButtonItem(title: "Done", style: .plain, target: self,
+                                   action: #selector(donePressed))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                            target: nil, action: nil)
+        bar.items = [flexibleSpace,done]
+        bar.sizeToFit()
         
+        textField.inputAccessoryView = bar
     }
     
-    private func backgroundСoloring(){
-        rgbPalette.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(1))
+    private func inputValuesKeyboard(textField: UITextField, slider: UISlider){
+        guard let text = textField.text else { return }
+        
+        if let currentValue = Float(text) {
+            slider.value = currentValue
+        }
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        inputValuesKeyboard(textField: redTextField, slider: redSlider)
+        inputValuesKeyboard(textField: greenTextField, slider: greenSlider)
+        inputValuesKeyboard(textField: blueTextField, slider: blueSlider)
+        
+        updatingValuesRGB()
+        view.endEditing(true)
+    }
+    
+    @objc func donePressed(){
+        inputValuesKeyboard(textField: redTextField, slider: redSlider)
+        inputValuesKeyboard(textField: greenTextField, slider: greenSlider)
+        inputValuesKeyboard(textField: blueTextField, slider: blueSlider)
+        
+        updatingValuesRGB()
+        view.endEditing(true)
+    }
+    
 }
-
